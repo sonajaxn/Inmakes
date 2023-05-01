c15ce0bfde0aff7b49a08f77a072dda55661f668
sonajaxn <sonajaison6@gmail.com>
2023-05-01 16:13:30 +0530

    tkinter

---
+++ Tkinter/tkinter_challenge.py
@@ -0,0 +1,6 @@
+from tkinter import *
+root=Tk()
+Label(root,text="Hi Sona").pack()
+Button(root,text="OKAY",fg="yellow",bg="black").pack()
+
+root.mainloop()
---
+++ Tkinter/tkinter_grid.py
@@ -0,0 +1,13 @@
+from tkinter import *
+root=Tk()
+l1=Label(root,text="welcome")
+l2=Label(root,text="user id")
+entry1=Entry(root)
+entry2=Entry(root)
+
+l1.grid(row=0,column=0)
+entry1.grid(row=0,column=1)
+l2.grid(row=1,column=0)
+entry2.grid(row=1,column=1)
+
+root.mainloop()
